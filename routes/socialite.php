483d81ea062cb949f19f0b4ec9da8b499edb521e
Atomjoy <atomjoy.official@gmail.com>
2024-05-21 13:35:09 +0200

    github login update

--- routes/socialite.php
+++ routes/socialite.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleLoginController;
+use App\Http\Controllers\GithubLoginController;
 
 Route::get('/google/redirect', [GoogleLoginController::class, 'redirect'])->name('google.redirect');
 Route::get('/google/callback', [GoogleLoginController::class, 'callback'])->name('google.callback');
 Route::get('/google/oauth', [GoogleLoginController::class, 'oauth'])->name('google.oauth');
 Route::get('/google/logout', [GoogleLoginController::class, 'logout'])->name('google.logout');
+
+Route::get('/github/redirect', [GithubLogin::class, 'redirect'])->name('github.redirect');
+Route::get('/github/callback', [GithubLogin::class, 'callback'])->name('github.callback');
+Route::get('/github/logout', [GithubLogin::class, 'logout'])->name('github.logout');
